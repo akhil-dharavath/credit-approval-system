39f8285a8dc9b04884b721300b38c8a0a18dbd95
akhil <86508192+akhildgithub@users.noreply.github.com>
2023-11-04 14:26:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorhandler");
+const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
