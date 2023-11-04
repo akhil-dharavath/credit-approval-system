12627bb132e59e64f17ce4949dd2ab46c3e0b416
akhil <86508192+akhildgithub@users.noreply.github.com>
2023-11-04 15:08:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 const app = express();
 
 connectDb();
 const port = process.env.PORT || 5000
 
-// middlewares
 app.use(express.json());
+app.use(cors());
+
+// middlewares
 app.use("/loan", require("./routes/loanRoutes"));
 app.use("/customer", require("./routes/customerRoutes"));
 app.use("*",(req,res)=>{res.status(404); throw new Error('Endpoint not found')});
